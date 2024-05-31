59adf67d3cdbc5ab7022e94c2b978be3a1f67a94
DarshanBhalani-Amnex <165637028+DarshanBhalani-Amnex@users.noreply.github.com>
2024-05-31 17:27:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Temperature_Producer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            List<long> pollNumbers = [2442,3254,1248,3567,15497,55479,74,974,546,7912,8794,1247,1246,79461,39,313,544,68,154,8478];
+
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+            var config = new ProducerConfig
+            {
+                BootstrapServers = $"{configuration["BootstrapService:Server"]}:{configuration["BootstrapService:Port"]}"
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                while (true)
+                {
+                    List<TemperatureModel> dataModels = new List<TemperatureModel>();
+
+                    foreach (var v in pollNumbers)
+                    {
+                        dataModels.Add(new TemperatureModel
+                        {
+                            PollNumber = v,
+                            Temperature = randomTemperature(),
+                            TimeStamp = DateTime.Now,
+                            Area = ""
+                        });
+                    }
+
+                    try
+                    {
+                        await producer.ProduceAsync(configuration["BootstrapService:Topic"], new Message<Null, string> { Value = JsonConvert.SerializeObject(dataModels) });
+                        await Console.Out.WriteLineAsync(JsonConvert.SerializeObject(dataModels));
+                        await Console.Out.WriteLineAsync();
+                    }
+                    catch (ProduceException<Null, string> e)
+                    {
+                        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        private static int randomTemperature()
+        {
+            int max = 55;
+            Random random = new Random();
+            return random.Next(30, max + 1);
+        }
+    }
+}
---
+++ TemperatureModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Temperature_Producer
+{
+    internal class TemperatureModel
+    {
+        public int Temperature { get; set; }
+        public long PollNumber { get; set; }
+        public string Area { get; set; }
+        public DateTime TimeStamp { get; set; } 
+    }
+}
